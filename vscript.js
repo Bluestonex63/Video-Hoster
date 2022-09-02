f32ab5d3c8450c545f165dafa7f8c18352102d11
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-09-02 01:08:11 +0100

    Update vscript.js

--- vscript.js
+++ vscript.js
@@ -2,11 +2,11 @@ let body = document.querySelector("body")
 let errormsg = document.createElement("p")
 body.appendChild(errormsg)
 check = function() {
-    if ([].includes(document.querySelector("#videosearch").value)) {
+    if (["Laser Arena Attempts"].includes(document.querySelector("#videosearch").value)) {
         errormsg.innerHTML = ""
         let video = document.createElement("video")
         video.src = `C:/Users/filze/Desktop/Python/Videos/${document.querySelector("#videosearch").value}.mp4`
-        document.appendChild(video)
+        body.appendChild(video)
     } else {
         errormsg.innerHTML = "Invalid Video"
     }
