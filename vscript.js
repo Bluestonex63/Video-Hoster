02f45f5414ad00a63f309f8a4b7a299cb2c85791
Bluestonex63 <103859845+Bluestonex63@users.noreply.github.com>
2022-09-02 01:06:51 +0100

    Update vscript.js

--- vscript.js
+++ vscript.js
@@ -5,7 +5,7 @@ check = function() {
     if ([].includes(document.querySelector("#videosearch").value)) {
         errormsg.innerHTML = ""
         let video = document.createElement("video")
-        video.src = `videos/${document.querySelector("#videosearch").value}.mp4`
+        video.src = `C:/Users/filze/Desktop/Python/Videos/${document.querySelector("#videosearch").value}.mp4`
         document.appendChild(video)
     } else {
         errormsg.innerHTML = "Invalid Video"
